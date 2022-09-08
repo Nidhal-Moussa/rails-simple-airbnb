57fc104a5f191fa47eec6690f36a577fdf26620d
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-09 00:30:52 +0200

    resolve delete problem and emprove front end

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -33,7 +33,7 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :description, :price_per_night, :number_of_guests, :address)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 
   def find_flat
